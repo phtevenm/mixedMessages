f6abf6a27423152e9e3eb33c6af921e9c2abb9ce
Stephen McGarry <stephen.mcgarry@hotmail.com>
2021-08-08 13:28:24 +0100

    Added 'signs' object

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,8 +1,16 @@
-let message = "Test"
+let message = 'Test';
 
 let signs = {
-    1: "Aries",
-    2: "Taurus",
-    3: 
-
-}
+	1: 'Aquarius',
+	2: 'Pisces',
+	3: 'Aries',
+	4: 'Taurus',
+	5: 'Gemini',
+	6: 'Cancer',
+	7: 'Leo',
+	8: 'Virgo',
+	9: 'Libra',
+	10: 'Scorpio',
+	11: 'Sagittarius',
+	12: 'Capricorn',
+};
